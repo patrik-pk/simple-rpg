7edca51f37af01d1e2798b54a774e4f73caaeb28
PatrikP <patrik.pukanec@gmail.com>
2020-08-06 01:05:47 +0200

    added items reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import characterReducer from './characterReducer'
 import dungeonReducer from './dungeonReducer'
+import itemsReducer from './itemsReducer'
 
 export default combineReducers({
     character: characterReducer,
-    dungeon: dungeonReducer
+    dungeon: dungeonReducer,
+    items: itemsReducer
 })
