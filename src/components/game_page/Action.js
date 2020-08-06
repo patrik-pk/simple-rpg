32fa209dc47169d9490477bc0569175188a993c1
PatrikP <patrik.pukanec@gmail.com>
2020-08-06 18:34:49 +0200

    converting Action.js into functional component

--- src/components/game_page/Action.js
+++ src/components/game_page/Action.js
@@ -1,76 +1,45 @@
-
-import React from "react"
-
+import React, { useState } from "react"
 import mainData from "../../data/_mainData"
 
-class Action extends React.Component {
-    // this is a Player Attack Button (used in Player.js)
+function Action(props) {
+    
+    const [hovered, setHovered] = useState(false)
 
-    state = {
-        data: {
-            type: null,
-            strength: null,
-            text: null,
-        },
-        dodge: 0,
-        isHovered: false
+    const handleHover = type => {
+        if (type === "Enter") setHovered(true)
+        if (type === "Leave") setHovered(false)
     }
 
-    componentDidMount() {
-        this.setState({ data: this.props.data, dodge: 0 })
-    }
-
-    componentWillUnmount() { 
-        this.setState({ 
-            isHovered: false 
-        }) 
-    }
-
-    // setState after Enemy fully loads
-    componentDidUpdate(prevProps) {
-        if (prevProps.dodge !== this.props.dodge) {
-            this.setState({ dodge: this.props.dodge })
+    const activeClass = hovered ? " active" : ""
+    
+    // chance to hit on Action hover
+    const hitChanceMult = () => {
+        switch(props.data.strength) {
+            case "light": return mainData.playerBase.attackTypes.hitChance.light
+            case "medium": return mainData.playerBase.attackTypes.hitChance.medium
+            case "strong": return mainData.playerBase.attackTypes.hitChance.strong
+            default: return 1;
         }
     }
+    const chanceToHit = (100 - (props.dodge * hitChanceMult())).toFixed(2)
 
-    handleClick = type => {
-        if(type === "Enter") { this.setState({ isHovered: true }) }
-        if (type === "Leave") { this.setState({ isHovered: false }) }
-    }
-
-    render() {
-        const activeClass = this.state.isHovered ? " active" : ""
-
-        
-        // chance to hit on Action hover
-        const hitChanceMult = () => {
-            switch(this.state.data.strength) {
-                case "light": return mainData.playerBase.attackTypes.hitChance.light
-                case "medium": return mainData.playerBase.attackTypes.hitChance.medium
-                case "strong": return mainData.playerBase.attackTypes.hitChance.strong
-                default: return 1;
-            }
-        }
-        const chanceToHit = (100 - (this.state.dodge * hitChanceMult())).toFixed(2)
-
-        return (
-            <div className="action">
-                <button 
-                onClick={() => this.props.gameManager(this.state.data.type, this.state.data.strength)}
-                onMouseEnter={() => this.handleClick("Enter")}
-                onMouseLeave={() => this.handleClick("Leave")}
-                >
-                    <div className={"hit_chance" + activeClass}>
-                        <p>Hit chance: </p>
-                        <p>{chanceToHit + "%"}</p>
-                    </div>
-                    <div className="icon" id={this.state.data.id}>
-                        { this.state.data.icon }
-                    </div>
-                </button>
-            </div>
-        )
-    }
+    return (
+        <div className="action">
+            <button 
+            onClick={() => this.props.gameManager(props.data.type, props.data.strength)}
+            onMouseEnter={() => handleHover("Enter")}
+            onMouseLeave={() => handleHover("Leave")}
+            >
+                <div className={"hit_chance" + activeClass}>
+                    <p>Hit chance: </p>
+                    <p>{chanceToHit + "%"}</p>
+                </div>
+                <div className="icon" id={props.data.id}>
+                    { props.data.icon }
+                </div>
+            </button>
+        </div>
+    )
 }
 
 export default Action
