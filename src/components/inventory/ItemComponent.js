be620bc869c6b3bf8aca922e9775e5ee4285fb77
PatrikP <patrik.pukanec@gmail.com>
2020-08-08 20:28:08 +0200

    rendering an item remaked

--- src/components/inventory/ItemComponent.js
+++ src/components/inventory/ItemComponent.js
@@ -1,37 +1,116 @@
 import React from "react"
-
-import itemRender from "../../logic/itemRender"
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import "../../styles/item/item.css"
 
-export default function ItemComponent(props) {
-
-    // TODO:
-    // - vyresit item handle click
+// TODO: vyresit item handle click
+function ItemComponent(props) {
 
     // If item has data & isn't of type 'Empty' (for shop items) render item
     if (props.data && props.data.type !== "Empty") {
 
-        // render values
-        const rv = itemRender(props)
+        // Destructure from props
+        const { data, equippedItems } = props
+        const { type, /*destination: dest,*/ rarity, goldValue, stats, bonuses, imgSrc, isSelected } = data
 
-        return (
-            
-            <li className={`item_container ${rv.rarityClass} ${rv.selectedClass}`}>
-                <div
-                className={`item ${rv.selectedClass}`}
-                onClick={props.handleClick ? () => props.handleClick(this) : null} 
-                >
-                    <img alt="" src={props.data.imgSrc ? props.data.imgSrc : null}/>
-                    { props.data ?
+        // Classes
+        const rarityClass = rarity.toLowerCase()
+        const selectedClass = isSelected ? " active" : ""
+
+        // Compared & Current Item (compared item has same type, e.g. compare bow with bow)
+        const comparedItem = equippedItems.filter(item => item.type === type)[0]
+        const currentItem = data
+
+        // Single Value Comparison
+        const compareSingle = (compared, current) => {
+            const diff = current - compared
+
+            if (diff > 0) return { value: '+' + Math.abs(diff), color: "green" }
+            else if (diff < 0) return { value: '-' + Math.abs(diff), color: "red" }
+            else if (diff === 0) return { value: diff, color: "yellow" }
+        }
+
+        // Compare Stat and Level
+        const comparedStat = compareSingle(comparedItem.stats.value, currentItem.stats.value)
+        const comparedLevel = compareSingle(comparedItem.level, currentItem.level)
+
+        // Compare Bonuses
+        const comparedBonuses = (() => {
+            const compBonuses = comparedItem.bonuses
+            const currBonuses = currentItem.bonuses
+            let diffArray = []
+
+            // Loop through current and nested bonuses,
+            currBonuses.forEach(current => {
+                const currName = current.name
+                const currVal = current.value
+
+                compBonuses.forEach(compared => {
+                    const compName = compared.name
+                    const compVal = compared.value
+
+                    // if they match, calculate the diff and push to diffArray
+                    if (currName === compName) {
+                        const diff = currVal - compVal
+
+                        if (diff > 0) diffArray.push({ value: '+' + Math.abs(diff), color: "green" })
+                        else if (diff < 0) diffArray.push({ value: '-' + Math.abs(diff), color: "red" })
+                        else if (diff === 0) diffArray.push({ value: diff, color: "yellow" })
+                    }
+                })
+            })
+
+            return diffArray
+        })()
+
+        // Destination Check
+        //const destCheck = (dest === "Inventory" || dest === "Shop" || dest === "Game") ? true : false
+
+        // Item Name
+        const itemName = (() => {
+            const name = rarity + " " + type + " (" + currentItem.level + ")"
+            return (
+                <div className="name_container">
+                    <p id="name">{name}</p>
+                    <p style={{ color: comparedLevel.color }}>{comparedLevel.value}</p>
+                </div>
+            )
+        })()
+
+        // Item Stat
+        const itemStat = <div className="stat_container">
+            <p>{stats.statName + ": " + stats.value}</p>
+            <p style={{ color: comparedStat.color }}>{comparedStat.value}</p>
+        </div>
+
+        // Item Bonuses
+        const itemBonuses = bonuses.map((bonus, i) => {
+            return (
+                <div key={i} className="bonus_container">
+                    <p>{bonus.displayedName + ": " + bonus.value}</p>
+                    <p style={{ color: comparedBonuses[i].color }}>{comparedBonuses[i].value}</p>
+                </div>
+            )
+        })
+
+        // RENDER
+        return ( 
+            <li className={`item_container ${rarityClass} ${selectedClass}`}>
+                <div className={`item ${selectedClass}`} onClick={props.handleClick ? () => props.handleClick(this) : null} >
+
+                    {/* Icon */}
+                    <img alt="" src={imgSrc ? imgSrc : null}/>
+
+                    {/* Info */}
                     <div className='stats'>
-                        { rv.name }
-                        { rv.stat }
+                        { itemName }
+                        { itemStat }
                         <div className="bonuses">
-                        { rv.bonuses }
+                        { itemBonuses }
                         </div>
-                        <p id="value"><span id="value_heading">Value:</span> {rv.value}</p>
+                        <p id="value"><span id="value_heading">Value:</span> {goldValue}</p>
                     </div>
-                    : null }
+
                 </div>
             </li>
         )
@@ -39,3 +118,13 @@ export default function ItemComponent(props) {
     // or render an empty item 
     return <li className="item_container"></li>
 }
+
+ItemComponent.propTypes = {
+    equippedItems: PropTypes.array.isRequired,
+}
+
+const mapStateToProps = state => ({
+    equippedItems: state.items.equippedItems
+})
+
+export default connect(mapStateToProps)(ItemComponent)
