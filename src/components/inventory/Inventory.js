709bfa699392075563a9ed58a97deccecd6a603a
PatrikP <patrik.pukanec@gmail.com>
2020-08-08 17:47:26 +0200

    converting Inventory.js into Redux

--- src/components/inventory/Inventory.js
+++ src/components/inventory/Inventory.js
@@ -11,143 +11,162 @@ import Stat from "../Stat"
 import levelTresholds from '../../data/levelTresholds'
 import "../../styles/inventory/inventory.css"
 
-class Inventory extends React.Component {
+function Inventory(props) {
 
-    componentWillUnmount() {
-        this.props.inventoryOnUnmount()
+    // Destructure from props
+    const { equippedItems, invItems, shopItems } = props.items
+    const { currentLevel, experience, gold, diamonds } = props.character
+    const { maxHp, armor, meleeDamage, rangedDamage, critChance, blockChance, bonuses } = props.player
+
+    /*componentWillUnmount() {
+        props.inventoryOnUnmount()
+    }*/
+
+    
+    // Slice item's into a 6 item array and put them into the InventoryRow Component
+    const getItems = (items, min, max) => {
+        const sliced = items.slice(min, max) 
+        let newItems = []
+
+        // Loop through max-min (6),
+        for(let i = 0; i < max-min; i++) {
+            // if item at index of i exists, put it into array
+            if(sliced[i]) newItems.push(sliced[i]) 
+            // if it doesn't push null into the array
+            else newItems.push(null)
+        }
+
+        return newItems
     }
 
-    render() {
-        const equippedItems = this.props.equippedItems
-        const invItems = this.props.invItems
-    
-        // Slice item's into a 6 item array and put them into the InventoryRow Component
-        const getItems = (type, min, max) => {
-            if(type === "equipped") return equippedItems.slice(min, max) 
-            else if (type === "inventory") return invItems.slice(min, max)
+    // Custom map for creating 6 InventoryRow components
+    const mappedRows = () => {
+        const mapped = []
+
+        for(let i = 0; i < 6; i++) {
+            mapped.push(<InventoryRow key={i} itemsProp={getItems(invItems, i * 6, (i * 6) + 6)} itemHandleClick={props.itemHandleClick} {...props} />)
         }
 
-        // Custom map for creating 6 InventoryRow components
-        const mappedRows = () => {
-            const mapped = []
+        return mapped
+    }
 
-            for(let i = 0; i < 6; i++) {
-                mapped.push(<InventoryRow key={i} items={getItems("inventory", i * 6, (i * 6) + 6)} itemHandleClick={this.props.itemHandleClick} {...this.props} />)
+    // Filter out selected Inventory and Shop items
+    const selectedInvItems = invItems.filter(item => item.isSelected)
+    const selectedShopItems = shopItems.filter(item => item.isSelected)
+
+    // Set active classes
+    const equipActive = selectedInvItems.length === 1 ? "active" : "null"
+    const sellActive = selectedInvItems.length >= 1 ? "active" : "null"
+    const rerollActive = props.currency.diamonds >= 1 ? "active" : "null"
+
+    // If Player has 1 selected shop item, has space in Inventory 
+    // and has money to buy that items, return active for buy btn class
+    const buyActive = () => {
+        if (selectedShopItems.length === 1 && invItems.length <= 35) {
+            const selectedItem = selectedShopItems[0]
+            if(props.currency.gold >= selectedItem.goldValue) {
+                return "active"
             }
-
-            return mapped
         }
-    
-        const selectedInvItems = this.props.invItems.filter(item => item.isSelected)
-        const selectedShopItems = this.props.shopItems.filter(item => item.isSelected)
+        return ''
+    }
 
-        const equipActive = selectedInvItems.length === 1 ? "active" : "null"
-        const sellActive = selectedInvItems.length >= 1 ? "active" : "null"
-        const rerollActive = this.props.currency.diamonds >= 1 ? "active" : "null"
+    // Level progress
+    const levelProgress = (experience / levelTresholds[currentLevel].xp * 100).toFixed(2)
 
-        const buyActive = () => {
-            if (selectedShopItems.length === 1 && invItems.length <= 35) {
-                const selectedItem = selectedShopItems[0]
-                if(this.props.currency.gold >= selectedItem.goldValue) {
-                    return "active"
-                } else return ""
-            } else return ""
-        }
-
-        const level = this.props.currentLevel
-        const levelProgress = (this.props.experience / levelTresholds[level].xp * 100).toFixed(2)
-    
-        return(
-            <div className="inventory">
-                <div className="top">
-                    <div className="left">
-                        <div className="current_equipment">
-                            <div className="wrapper">
-                                <InventoryRow items={getItems("equipped", 0, 6)} itemHandleClick={this.props.itemHandleClick} {...this.props}/>                                        
-                                <InventoryRow items={getItems("equipped", 6, 12)} itemHandleClick={this.props.itemHandleClick} {...this.props}/>                                        
-                            </div>
-                        </div>
-                        <div className="player_stats">
-                            <div className="wrapper">
-                                <h4 className="name">Stats</h4>
-                                <hr/>
-                                <ul>
-                                    <Stat name="HP:" value={this.props.player.maxHp}/>
-                                    <Stat name="Armor:" value={this.props.player.armor}/>
-                                    <Stat name="M-DMG:" value={this.props.player.meleeDamage}/>
-                                    <Stat name="R-DMG:" value={this.props.player.rangedDamage}/>
-                                    <Stat name="Crit(%):" value={this.props.player.critChance}/>
-                                    <Stat name="Block(%):" value={this.props.player.blockChance} />
-                                    <br/>
-                                    <Stat name="Beasts:" value={this.props.player.bonuses[0].value} />
-                                    <Stat name="Dragons:" value={this.props.player.bonuses[1].value} />
-                                    <Stat name="Insect:" value={this.props.player.bonuses[2].value} />
-                                    <Stat name="Monsters:" value={this.props.player.bonuses[3].value} />
-                                    <Stat name="Reptiles:" value={this.props.player.bonuses[4].value} />
-                                </ul>
-                            </div>
+    return(
+        <div className="inventory">
+            
+            <div className="top">
+                <div className="left">
+                    <div className="current_equipment">
+                        <div className="wrapper">
+                            <InventoryRow itemsProp={getItems(equippedItems, 0, 6)} itemHandleClick={props.itemHandleClick} {...props}/>                                        
+                            <InventoryRow itemsProp={getItems(equippedItems, 6, 12)} itemHandleClick={props.itemHandleClick} {...props}/>                                        
                         </div>
                     </div>
-                    <div className="right">
-                        <div className="items_container">
-                            <div className="wrapper">
-                                <div className="items">
-                                    { mappedRows() }
-                                </div>
-                            </div>
-                            <div className="options">
-                                <div className="wrapper">
-                                    <button className={"equip_btn " + equipActive} onClick={this.props.equipItem}>Equip</button>
-                                    <button className={"sell_btn " + sellActive} onClick={this.props.sellItem}>Sell</button>
-                                </div>
-                            </div>
+                    <div className="player_stats">
+                        <div className="wrapper">
+                            <h4 className="name">Stats</h4>
+                            <hr/>
+                            <ul>
+                                <Stat name="HP:" value={maxHp}/>
+                                <Stat name="Armor:" value={armor}/>
+                                <Stat name="M-DMG:" value={meleeDamage}/>
+                                <Stat name="R-DMG:" value={rangedDamage}/>
+                                <Stat name="Crit(%):" value={critChance}/>
+                                <Stat name="Block(%):" value={blockChance} />
+                                <br/>
+                                <Stat name="Beasts:" value={bonuses[0].value} />
+                                <Stat name="Dragons:" value={bonuses[1].value} />
+                                <Stat name="Insect:" value={bonuses[2].value} />
+                                <Stat name="Monsters:" value={bonuses[3].value} />
+                                <Stat name="Reptiles:" value={bonuses[4].value} />
+                            </ul>
                         </div>
                     </div>
                 </div>
-
-                <div className="bottom">
-                    <div className="shop_container">
+                <div className="right">
+                    <div className="items_container">
                         <div className="wrapper">
                             <div className="items">
-                                <ul>
-                                    <ItemComponent data={this.props.shopItems[0]} handleClick={this.props.itemHandleClick} {...this.props}/>     
-                                    <ItemComponent data={this.props.shopItems[1]} handleClick={this.props.itemHandleClick} {...this.props}/>                                      
-                                    <ItemComponent data={this.props.shopItems[2]} handleClick={this.props.itemHandleClick} {...this.props}/>                                      
-                                </ul>
-                            </div>
-                            <div className="options">
-                                <button className={"reroll_btn " + rerollActive} onClick={this.props.reroll}>Roll (1)</button>
-                                <button className={"buy_btn " + buyActive()} onClick={this.props.buyItem}>Buy</button>
+                                { mappedRows() }
                             </div>
                         </div>
-                    </div>
-                    
-                    <div className="money_container">
-                        <div className="info">
-                            <p id="level">Level {level}</p>
-                            <p>{levelProgress}%</p>
-                        </div>
-                        <div className="money">
-                            <p id="gold">Gold: {this.props.currency.gold}</p>
-                            <p id="diamonds">Diamonds: {this.props.currency.diamonds}</p>
+                        <div className="options">
+                            <div className="wrapper">
+                                <button className={"equip_btn " + equipActive} onClick={props.equipItem}>Equip</button>
+                                <button className={"sell_btn " + sellActive} onClick={props.sellItem}>Sell</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <Link to="/menu" className="menu_btn back_btn">Back</Link>                        
             </div>
-        )
-    }
+
+            <div className="bottom">
+                <div className="shop_container">
+                    <div className="wrapper">
+                        <div className="items">
+                            <ul>
+                                <ItemComponent data={shopItems[0]} handleClick={props.itemHandleClick} {...props}/>     
+                                <ItemComponent data={shopItems[1]} handleClick={props.itemHandleClick} {...props}/>                                      
+                                <ItemComponent data={shopItems[2]} handleClick={props.itemHandleClick} {...props}/>                                      
+                            </ul>
+                        </div>
+                        <div className="options">
+                            <button className={"reroll_btn " + rerollActive} onClick={props.reroll}>Roll (1)</button>
+                            <button className={"buy_btn " + buyActive()} onClick={props.buyItem}>Buy</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="money_container">
+                    <div className="info">
+                        <p id="level">Level {currentLevel}</p>
+                        <p>{levelProgress}%</p>
+                    </div>
+                    <div className="money">
+                        <p id="gold">Gold: {gold}</p>
+                        <p id="diamonds">Diamonds: {diamonds}</p>
+                    </div>
+                </div>
+            </div>
+
+            <Link to="/menu" className="menu_btn back_btn">Back</Link>                        
+    </div>
+    )
 }
 
 Inventory.propTypes = {
-    currentLevel: PropTypes.number.isRequired,
-    experience: PropTypes.number.isRequired,
+    character: PropTypes.object.isRequired,
+    items: PropTypes.object.isRequired,
+    player: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state => ({
-    currentLevel: state.character.currentLevel,
-    experience: state.character.experience
+    character: state.character,
+    items: state.items,
+    player: state.player
 })
 
 export default connect(mapStateToProps)(Inventory)
--- src/components/inventory/InventoryRow.js
+++ src/components/inventory/InventoryRow.js
@@ -2,26 +2,12 @@ import React from "react"
 import ItemComponent from "./ItemComponent"
 
 export default function InventoryRow(props) {
- 
-    // Custom map function to map items from props
-    const mapItems = () => {
-        const mapped = []
-        // Loop 6 times
-        for(let i = 0; i < 6; i++) {
-            // If the item[i] exists, add <ItemComponent/> with data of item[i] to array
-            if (props.items[i]) {
-                mapped.push(<ItemComponent key={props.items[i].key} data={props.items[i]} {...props} handleClick={props.itemHandleClick} />)
-            }
-            // else add <ItemComponent/> with null data to array - empty item
-            else mapped.push(<ItemComponent key={i} data={null} />)
-        }
 
-        return mapped
-    }
+    const mapped =  props.itemsProp.map((item, i) => <ItemComponent key={i} data={item} {...props} handleClick={props.itemHandleClick} />)
 
     return (
         <ul>
-        { mapItems() }
+        { mapped }
         </ul>
     )
 }
