9ee82a5c3585be558aebe771de91c2c217891a4a
PatrikP <patrik.pukanec@gmail.com>
2020-09-02 17:26:00 +0200

    craftable items remaked and simplified

--- src/shared/data/craftable_items/craftableItems.js
+++ src/shared/data/craftable_items/craftableItems.js
@@ -1,46 +1,75 @@
-import mythic from './mythic'
-import aquatic from './aquatic'
-import avian from './avian'
-import dinosaur from './dinosaur'
-import insect from './insect'
-import wildlife from './wildlife'
-import reptile from './reptile'
+import generateItem from '../../generateItem'
+import possibleItems from '../possibleItems'
+import data from './data'
 
-// Exported Craftable Items - I will clean this up soon don't worry
-const craftableItems = [
+const craftableItems = []
 
-    // Low
-    [
-        mythic.low,
-        aquatic.low,
-        avian.low,
-        dinosaur.low,
-        insect.low,
-        wildlife.low,
-        reptile.low
-    ],
+const createBonuses = values => ([
+    { name: 'aquatic', value: values[0] },
+    { name: 'avian', value: values[1] },
+    { name: 'dinosaur', value: values[2] },
+    { name: 'insect', value: values[3] },
+    { name: 'reptile', value: values[4] },
+    { name: 'wildlife', value: values[5] },
+])
 
-    // Medium
-    [
-        mythic.medium,
-        aquatic.medium,
-        avian.medium,
-        dinosaur.medium,
-        insect.medium,
-        wildlife.medium,
-        reptile.medium
-    ],
+const possibleItemsArray = Object.values(possibleItems)
 
-    // High
-    [
-        mythic.high,
-        aquatic.high,
-        avian.high,
-        dinosaur.high,
-        insect.high,
-        wildlife.high,
-        reptile.high
-    ]
-]
+// loop through data and create complex craftable items array,
+// which looks like this:
+// [
+//     3x levelType (low, medium, high)
+//     [
+//         7x rarityType (mythic, aquatic, avian, etc.)
+//         [
+//             12x item for each type (bow, sword, helmet, etc.)
+//             [
+//                 {
+//                     dropsNeeded: [], drops needed array
+//                     item: {} item object
+//                 }
+//             ]
+//         ]
+//     ]
+// ]
+
+data.forEach((levelType, levelTypeIndex) => {
+    const { levelOfItems, rarities } = levelType
+    craftableItems.push([])
+
+    rarities.forEach((rarityType, rarityIndex) => {
+        const { bonusValues, rarity, id, neededDrops, iconIndexes } = rarityType
+        craftableItems[levelTypeIndex].push([])
+
+        for(let i = 0; i < 12; i++) {
+
+            const specific = {
+                rarity,
+                itemType: { 
+                    type: possibleItemsArray[i], 
+                    iconIndex: iconIndexes[i] 
+                },
+                bonuses: createBonuses(bonusValues)
+            }
+            
+            const item = generateItem(
+                levelOfItems,
+                'Crafting',
+                `${id}-${i}`,
+                'Classic',
+                specific
+            )
+
+            const dropsNeeded = neededDrops.drops.map(drop => 
+                ({ name: drop, amount: neededDrops.amount })
+            )
+
+            craftableItems[levelTypeIndex][rarityIndex].push({
+                item,
+                dropsNeeded, 
+            })
+        }
+    })
+})
 
 export default craftableItems
---
+++ src/shared/data/craftable_items/data.js
@@ -0,0 +1,296 @@
+import rarities from '../rarities'
+import iconIndexes from './iconIndexes'
+
+export default [
+
+    // Low Level
+    {
+        levelOfItems: 5,
+        rarities: [
+            // Mythic
+            {
+                bonusValues: [20, 20, 20, 20, 20, 20],
+                rarity: rarities.mythic,
+                id: 'mythic-low',
+                neededDrops: {
+                    amount: 10,
+                    drops: [
+                        'small aquatic soul',
+                        'small aqua meat',
+                        'small avian soul',
+                        'small feather',
+                        'small dinosaur soul',
+                        'small fossil',
+                        'small insect soul',
+                        'weak poison',
+                        'small wildlife soul',
+                        'soft wildlife leather',
+                        'small reptile soul',
+                        'soft reptile leather',
+                    ]
+                },
+                iconIndexes: iconIndexes.mythic
+            },
+            // Aquatic
+            {
+                bonusValues: [75, 0, 0, 0, 0, 0],
+                rarity: rarities.aquatic,
+                id: 'aquatic-low',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['small aquatic soul', 'small aqua meat']
+                },
+                iconIndexes: iconIndexes.aquatic
+            },
+            // Avian
+            {
+                bonusValues: [0, 75, 0, 0, 0, 0],
+                rarity: rarities.avian,
+                id: 'avian-low',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['small avian soul', 'small feather']
+                },
+                iconIndexes: iconIndexes.avian
+            },
+            // Dinosaur
+            {
+                bonusValues: [0, 0, 75, 0, 0, 0],
+                rarity: rarities.dinosaur,
+                id: 'dinosaur-low',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['small dinosaur soul', 'small fossil']
+                },
+                iconIndexes: iconIndexes.dinosaur
+            },
+            // Insect
+            {
+                bonusValues: [0, 0, 0, 75, 0, 0],
+                rarity: rarities.insect,
+                id: 'insect-low',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['small insect soul', 'weak poison']
+                },
+                iconIndexes: iconIndexes.insect
+            },
+            // Reptile
+            {
+                bonusValues: [0, 0, 0, 0, 75, 0],
+                rarity: rarities.reptile,
+                id: 'reptile-low',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['small reptile soul', 'soft reptile leather']
+                },
+                iconIndexes: iconIndexes.reptile
+            },
+            // Wildlife
+            {
+                bonusValues: [0, 0, 0, 0, 0, 75],
+                rarity: rarities.wildlife,
+                id: 'wildlife-low',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['small wildlife soul', 'soft wildlife leather']
+                },
+                iconIndexes: iconIndexes.wildlife
+            },
+        ]
+    },
+
+    // Medium Level
+    {
+        levelOfItems: 15,
+        rarities: [
+            // Mythic
+            {
+                bonusValues: [20, 20, 20, 20, 20, 20],
+                rarity: rarities.mythic,
+                id: 'mythic-medium',
+                neededDrops: {
+                    amount: 10,
+                    drops: [
+                        'medium aquatic soul',
+                        'medium aqua meat',
+                        'medium avian soul',
+                        'medium feather',
+                        'medium dinosaur soul',
+                        'medium fossil',
+                        'medium insect soul',
+                        'medium poison',
+                        'medium wildlife soul',
+                        'medium wildlife leather',
+                        'medium reptile soul',
+                        'medium reptile leather',
+                    ]
+                },
+                iconIndexes: iconIndexes.mythic
+            },
+            // Aquatic
+            {
+                bonusValues: [75, 0, 0, 0, 0, 0],
+                rarity: rarities.aquatic,
+                id: 'aquatic-medium',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['medium aquatic soul', 'medium aqua meat']
+                },
+                iconIndexes: iconIndexes.aquatic
+            },
+            // Avian
+            {
+                bonusValues: [0, 75, 0, 0, 0, 0],
+                rarity: rarities.avian,
+                id: 'avian-medium',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['medium avian soul', 'medium feather']
+                },
+                iconIndexes: iconIndexes.avian
+            },
+            // Dinosaur
+            {
+                bonusValues: [0, 0, 75, 0, 0, 0],
+                rarity: rarities.dinosaur,
+                id: 'dinosaur-medium',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['medium dinosaur soul', 'medium fossil']
+                },
+                iconIndexes: iconIndexes.dinosaur
+            },
+            // Insect
+            {
+                bonusValues: [0, 0, 0, 75, 0, 0],
+                rarity: rarities.insect,
+                id: 'insect-medium',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['medium insect soul', 'medium poison']
+                },
+                iconIndexes: iconIndexes.insect
+            },
+            // Reptile
+            {
+                bonusValues: [0, 0, 0, 0, 75, 0],
+                rarity: rarities.reptile,
+                id: 'reptile-medium',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['medium reptile soul', 'medium reptile leather']
+                },
+                iconIndexes: iconIndexes.reptile
+            },
+            // Wildlife
+            {
+                bonusValues: [0, 0, 0, 0, 0, 75],
+                rarity: rarities.wildlife,
+                id: 'wildlife-medium',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['medium wildlife soul', 'medium wildlife leather']
+                },
+                iconIndexes: iconIndexes.wildlife
+            },
+        ]
+    },
+
+    // High Level
+    {
+        levelOfItems: 25,
+        rarities: [
+            // Mythic
+            {
+                bonusValues: [20, 20, 20, 20, 20, 20],
+                rarity: rarities.mythic,
+                id: 'mythic-high',
+                neededDrops: {
+                    amount: 10,
+                    drops: [
+                        'large aquatic soul',
+                        'large aqua meat',
+                        'large avian soul',
+                        'large feather',
+                        'large dinosaur soul',
+                        'large fossil',
+                        'large insect soul',
+                        'strong poison',
+                        'large wildlife soul',
+                        'tough wildlife leather',
+                        'large reptile soul',
+                        'tough reptile leather',
+                    ]
+                },
+                iconIndexes: iconIndexes.mythic
+            },
+            // Aquatic
+            {
+                bonusValues: [75, 0, 0, 0, 0, 0],
+                rarity: rarities.aquatic,
+                id: 'aquatic-high',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['large aquatic soul', 'large aqua meat']
+                },
+                iconIndexes: iconIndexes.aquatic
+            },
+            // Avian
+            {
+                bonusValues: [0, 75, 0, 0, 0, 0],
+                rarity: rarities.avian,
+                id: 'avian-high',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['large avian soul', 'large feather']
+                },
+                iconIndexes: iconIndexes.avian
+            },
+            // Dinosaur
+            {
+                bonusValues: [0, 0, 75, 0, 0, 0],
+                rarity: rarities.dinosaur,
+                id: 'dinosaur-high',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['large dinosaur soul', 'large fossil']
+                },
+                iconIndexes: iconIndexes.dinosaur
+            },
+            // Insect
+            {
+                bonusValues: [0, 0, 0, 75, 0, 0],
+                rarity: rarities.insect,
+                id: 'insect-high',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['large insect soul', 'strong poison']
+                },
+                iconIndexes: iconIndexes.insect
+            },
+            // Reptile
+            {
+                bonusValues: [0, 0, 0, 0, 75, 0],
+                rarity: rarities.reptile,
+                id: 'reptile-high',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['large reptile soul', 'tough reptile leather']
+                },
+                iconIndexes: iconIndexes.reptile
+            },
+            // Wildlife
+            {
+                bonusValues: [0, 0, 0, 0, 0, 75],
+                rarity: rarities.wildlife,
+                id: 'wildlife-high',
+                neededDrops: {
+                    amount: 25,
+                    drops: ['large wildlife soul', 'tough wildlife leather']
+                },
+                iconIndexes: iconIndexes.wildlife
+            },
+        ]
+    },
+]
